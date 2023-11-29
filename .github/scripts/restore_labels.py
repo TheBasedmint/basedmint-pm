3fda5743d815029d839fe115b06c494ac9b09718
jessiebroke <18238304+jessiebroke@users.noreply.github.com>
2023-11-29 16:30:25 -0500

    create artifact per repo affected

--- .github/scripts/restore_labels.py
+++ .github/scripts/restore_labels.py
@@ -36,7 +36,7 @@ def fetch_all_repositories(username, organization, token):
 if __name__ == "__main__":
     # Replace with your GitHub username, organization, and personal access token
     username = "JessieBroke"
-    organization = "codex-storage"
+    organization = "TheBasedmint"  # Correct organization name
     token = os.getenv("GH_PAT")
 
     # Fetch all repositories in the organization
@@ -48,4 +48,21 @@ if __name__ == "__main__":
         events = fetch_issue_events(username, organization, repository, token)
 
         if events:
-            # Save events to a JSON file for each reposi
+            # Save events to a JSON file for each repository
+            filename = f"{repository}_issue_events.json"
+            with open(filename, "w") as file:
+                json.dump(events, file)
+
+            # Restore labels based on deletion events for each repository
+            for event in events:
+                if event["event"] == "label":
+                    label_name = event["label"]["name"]
+                    # Restore the label using your preferred method (GitHub API, gh CLI, etc.)
+                    print(f"Restoring label: {label_name} in {repository}")
+                    # Add logic here to restore the label using the GitHub API or other methods
+
+            # Upload the JSON file as an artifact for each repository
+            os.system(f'echo "::set-output name=artifacts::{filename}"')
+            os.system(f'echo "::set-output name=artifact_paths::{filename}"')
+
+        print("\n")
