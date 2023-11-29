45ce1459321f529826dc94e57d29d77d5a230278
jessiebroke <18238304+jessiebroke@users.noreply.github.com>
2023-11-29 16:23:35 -0500

    create artifact per repo affected

--- .github/scripts/restore_labels.py
+++ .github/scripts/restore_labels.py
@@ -1,43 +1,3 @@
-import os
-import requests
-import json
-
-def fetch_issue_events(username, organization, repository, token):
-    # GitHub API endpoint for listing issue events
-    api_url = f"https://api.github.com/repos/{organization}/{repository}/issues/events"
-
-    # Fetch issue events
-    response = requests.get(api_url, headers={"Authorization": f"Bearer {token}"})
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to fetch issue events for {repository}. Status code: {response.status_code}")
-        return None
-
-def fetch_all_repositories(username, organization, token):
-    # GitHub API endpoint for listing repositories in the organization
-    api_url = f"https://api.github.com/orgs/{organization}/repos"
-
-    # Fetch repositories
-    response = requests.get(api_url, headers={"Authorization": f"Bearer {token}"})
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        return [repo["name"] for repo in response.json()]
-    else:
-        print(f"Failed to fetch repositories for {organization}. Status code: {response.status_code}")
-        return []
-
-# Replace with your GitHub username, organization, and personal access token
-username = "JessieBroke"
-organization = "codex-storage"
-token = os.getenv("GH_PAT")
-
-# Fetch all repositories in the organization
-repositories = fetch_all_repositories(username, organization, token)
-
 # Iterate over each repository and fetch issue events
 for repository in repositories:
     print(f"Fetching issue events for {repository}")
@@ -45,7 +5,8 @@ for repository in repositories:
 
     if events:
         # Save events to a JSON file for each repository
-        with open(f"{repository}_issue_events.json", "w") as file:
+        filename = f"{repository}_issue_events.json"
+        with open(filename, "w") as file:
             json.dump(events, file)
 
         # Restore labels based on deletion events for each repository
@@ -55,4 +16,12 @@ for repository in repositories:
                 # Restore the label using your preferred method (GitHub API, gh CLI, etc.)
                 print(f"Restoring label: {label_name} in {repository}")
                 # Add logic here to restore the label using the GitHub API or other methods
+
+        # Upload the JSON file as an artifact for each repository
+        os.system(f'echo "::set-output name=artifacts::{filename}"')
+
+        # Upload the JSON file as an artifact using GitHub Actions command
+        os.system(f'echo "::set-output name=artifacts::{filename}"')
+        os.system(f'echo "::set-output name=artifact_paths::{filename}"')
+
     print("\n")
