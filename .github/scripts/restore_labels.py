2e7f93326d81229ddfcbdff2917ac22023856fe6
jessiebroke <18238304+jessiebroke@users.noreply.github.com>
2023-11-29 20:34:44 -0500

    restore to commit 8f5701f

--- .github/scripts/restore_labels.py
+++ .github/scripts/restore_labels.py
@@ -2,63 +2,27 @@ import os
 import requests
 import json
 
-# Global variable for repositories
-repositories = []
+# Replace with your GitHub username, organization, repository, and personal access token
+username = "JessieBroke"
+organization = "TheBasedmint"
+repository = "test1"
+token = os.getenv("GH_PAT")
 
-def fetch_issue_events(username, organization, repository, token):
-    # GitHub API endpoint for listing issue events
-    api_url = f"https://api.github.com/repos/{organization}/{repository}/issues/events"
+# GitHub API endpoint for listing issue events
+api_url = f"https://api.github.com/repos/{organization}/{repository}/issues/events"
 
-    # Fetch issue events
-    response = requests.get(api_url, headers={"Authorization": f"Bearer {token}"})
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to fetch issue events for {repository}. Status code: {response.status_code}")
-        return None
+# Fetch issue events
+response = requests.get(api_url, headers={"Authorization": f"Bearer {token}"})
+events = response.json()
 
-def fetch_all_repositories(username, organization, token):
-    # GitHub API endpoint for listing repositories in the organization
-    api_url = f"https://api.github.com/orgs/{organization}/repos"
+# Save events to a JSON file
+with open("issue_events.json", "w") as file:
+    json.dump(events, file)
 
-    # Fetch repositories
-    response = requests.get(api_url, headers={"Authorization": f"Bearer {token}"})
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        return [repo["name"] for repo in response.json()]
-    else:
-        print(f"Failed to fetch repositories for {organization}. Status code: {response.status_code}")
-        return []
-
-if __name__ == "__main__":
-    # Replace with your GitHub username, organization, and personal access token
-    username = "JessieBroke"
-    organization = "TheBasedmint"  # Correct organization name
-    token = os.getenv("GH_PAT")
-
-    # Fetch all repositories in the organization
-    repositories = fetch_all_repositories(username, organization, token)
-
-    # Iterate over each repository and fetch issue events
-    for repository in repositories:
-        print(f"Fetching issue events for {repository}")
-        events = fetch_issue_events(username, organization, repository, token)
-
-        if events:
-            # Save events to a JSON file for each repository in the output directory
-            filename = f"output/{repository}_issue_events.json"
-            with open(filename, "w") as file:
-                json.dump(events, file)
-
-            # Restore labels based on deletion events for each repository
-            for event in events:
-                if event["event"] == "label":
-                    label_name = event["label"]["name"]
-                    # Restore the label using your preferred method (GitHub API, gh CLI, etc.)
-                    print(f"Restoring label: {label_name} in {repository}")
-                    # Add logic here to restore the label using the GitHub API or other methods
-
-    print("\n")
+# Restore labels based on deletion events
+for event in events:
+    if event["event"] == "label":
+        label_name = event["label"]["name"]
+        # Restore the label using your preferred method (GitHub API, gh CLI, etc.)
+        print(f"Restoring label: {label_name}")
+        # Add logic here to restore the label using the GitHub API or other methods
