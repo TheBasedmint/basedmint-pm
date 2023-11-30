790776cdb0642257144170dc6d2cf277a747ce49
jessiebroke <18238304+jessiebroke@users.noreply.github.com>
2023-11-29 21:22:27 -0500

    modify to capture all removed labels

--- .github/scripts/vacuum.py
+++ .github/scripts/vacuum.py
@@ -3,36 +3,26 @@ import requests
 import json
 from datetime import datetime, timedelta
 
-# Function to fetch issue events from GitHub API
-def fetch_issue_events(repository):
+# Replace with your GitHub username, organization, repository, and personal access token
+username = "JessieBroke"
+organization = "TheBasedmint"
+repositories = ["test1", "test2"]  # Add other repositories as needed
+token = os.getenv("GH_PAT")
+
+# Calculate the datetime 48 hours ago from now
+time_threshold = datetime.utcnow() - timedelta(hours=48)
+
+# Create a dictionary to store unique issues based on issue URL
+unique_issues = {}
+
+# Iterate through each repository
+for repository in repositories:
+    # GitHub API endpoint for listing issue events
     api_url = f"https://api.github.com/repos/{organization}/{repository}/issues/events"
+
+    # Fetch issue events
     response = requests.get(api_url, headers={"Authorization": f"Bearer {token}"})
-    return response.json() if response.status_code == 200 else None
-
-# Function to save issue events to a JSON file
-def save_issue_events(repository, events):
-    output_dir = "output"
-    os.makedirs(output_dir, exist_ok=True)
-    filename = os.path.join(output_dir, f"{repository}_issue_events.json")
-    with open(filename, "w") as file:
-        json.dump(events, file)
-
-# Function to restore labels based on deletion events
-def restore_labels(events):
-    for event in events:
-        if event["event"] == "label":
-            label_name = event["label"]["name"]
-            # Restore the label using your preferred method (GitHub API, gh CLI, etc.)
-            print(f"Restoring label: {label_name}")
-            # Add logic here to restore the label using the GitHub API or other methods
-
-# Function to filter issue events and extract unique issues
-def filter_and_extract(events):
-    # Calculate the datetime 48 hours ago from now
-    time_threshold = datetime.utcnow() - timedelta(hours=48)
-
-    # Create a dictionary to store unique issues based on issue URL
-    unique_issues = {}
+    events = response.json()
 
     # Iterate through each event in the list
     for event in events:
@@ -60,38 +50,11 @@ def filter_and_extract(events):
                         'issue_url': issue_url
                     }
 
-    # Convert the dictionary values to a list
-    filtered_data = list(unique_issues.values())
+# Convert the dictionary values to a list
+filtered_data = list(unique_issues.values())
 
-    # Write the filtered data to a new JSON file
-    with open('output/vacuum_data.json', 'w') as output_file:
-        json.dump(filtered_data, output_file, indent=2)
-
-if __name__ == "__main__":
-    # Replace with your GitHub username, organization, and personal access token
-    username = "JessieBroke"
-    organization = "TheBasedmint"
-    repositories = ["test1", "test2"]  # Add additional repositories here
-    token = os.getenv("GH_PAT")
-
-    for repository in repositories:
-        print(f"Fetching and processing issue events for {repository}")
-
-        # Fetch issue events
-        events = fetch_issue_events(repository)
-
-        if events:
-            # Save issue events to a file
-            save_issue_events(repository, events)
-
-            # Restore labels based on deletion events
-            restore_labels(events)
-
-    # Combine issue events from all repositories
-    combined_events = []
-    for repository in repositories:
-        with open(f'output/{repository}_issue_events.json', 'r') as file:
-            combined_events.extend(json.load(file))
-
-    # Filter and extract unique issues
-    filter_and_extract(combined_events)
+# Write the filtered data to a new JSON file
+output_path = os.path.join("output", "vacuum_data.json")
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+with open(output_path, 'w') as output_file:
+    json.dump(filtered_data, output_file, indent=2)
